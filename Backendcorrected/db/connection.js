57c4d16174a74a1336a475462655ebce7c6ab0d0
libinrajds <libinraj2020@gmail.com>
2025-05-15 13:04:28 +0530

    newcommit

--- Backendcorrected/db/connection.js
+++ Backendcorrected/db/connection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 require('dotenv').config()
 
 //Connecting db
-mongoose.connect(process.env.MongoDB_URL).then(()=>{
+mongoose.connect(process.env.mongodb_url).then(()=>{
     console.log('Connection established')
 }).catch(()=>{
     console.log('Connection error')
